571e4fa67ca4cffbd08fb52d9cb5bbc566bf2f91
jcalvin4 <97327327+jcalvin4@users.noreply.github.com>
2022-06-03 10:23:05 -0600

    Initial commit

---
+++ main.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+class main {
+    val scan = Scanner(System.`in`)
+    val read = scan.nextLine()
+    fun main(){
+        println("Welcome to the calcuverter!")
+        println("What do you need today?")
+        println("1. Calculator 2. Converter")
+        val response = read.toInt()
+    }
+}
